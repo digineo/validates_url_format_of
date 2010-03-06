5eb10f352bd56daa870eb265e5771929a62efaa6
Julian Kornberger <github.corny@digineo.de>
2010-03-06 19:22:56 +0100

    lots of configuration options added

--- init.rb
+++ init.rb
@@ -1,28 +1,70 @@
 module ValidatesUrlFormatOf
-  IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
-  REGEXP = %r{
-    \A
-    https?://                                        # http:// or https://
-    ([^\s:@]+:[^\s:@]*@)?                            # optional username:pw@
-    ( (xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
-        #{IPv4_PART}(\.#{IPv4_PART}){3} )            # or IPv4
-    (:\d{1,5})?                                      # optional port
-    ([/?]\S*)?                                       # optional /whatever or ?whatever
-    \Z
-  }iux
-
-  DEFAULT_MESSAGE     = 'does not appear to be a valid URL'
-  DEFAULT_MESSAGE_URL = 'does not appear to be valid'
+  
+  module Pattern
+    # Inspired by http://flanders.co.nz/2009/11/08/a-good-url-regular-expression-repost/s
+    
+    IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
+    IPv4      = /#{IPv4_PART}(\.#{IPv4_PART}){3}/
+    AUTH      = /[^\s:@]+:[^\s:@]*@/
+    DOMAIN    = /(xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}/
+    PORT      = /(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{0,3}|0)/ # 0-65535
+    QUERY     = /((\?([-\w~!$+|.,*:]|%[a-f\d{2}])+=?([-\w~!$+|.,*:=]|%[a-f\d]{2})*)(&([-\w~!$+|.,*:]|%[a-f\d{2}])+=?([-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*/
+    PATH      = /((\/([-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+/
+    FRAGMENT  = /#([-\w~!$+|.,*:=]|%[a-f\d]{2})*/
+  end
   
   def validates_url_format_of(*attr_names)
-    options = { :allow_nil => false,
-                :allow_blank => false,
-                :with => REGEXP }
-    options = options.merge(attr_names.pop) if attr_names.last.is_a?(Hash)
-
+    configuration = {
+      :allow_ip        => true,
+      :allow_auth      => true,
+      :allow_domain    => true,
+      :allow_port      => true,
+      :allow_query     => true,
+      :allow_fragment  => true,
+      :require_slash   => true,
+      :protocols       => ["https?"]
+    }
+    configuration.update(attr_names.extract_options!)
+    
+    # protocol
+    raise ArgumentError, "at least one protocol must be allowed" if configuration[:protocols].empty?
+    regexp = "(" + configuration[:protocols].join("|") + ")"
+    regexp << ":\\/\\/"
+    
+    # auth
+    if configuration[:allow_auth]
+      regexp << "(" << Pattern::AUTH.to_s << ")?"
+    end
+    
+    # ip or domain
+    parts = []
+    parts << Pattern::IPv4   if configuration[:allow_ip]
+    parts << Pattern::DOMAIN if configuration[:allow_domain]
+    raise ArgumentError, "ip or domain must be allowed" if parts.empty?
+    regexp << "(" << parts.join("|") << ")"
+    
+    # optional port
+    if configuration[:allow_port]
+      regexp << "(:" << Pattern::PORT.to_s << ")?"
+    end
+    
+    # path
+    regexp << Pattern::PATH.to_s
+    
+    # optional query
+    if configuration[:allow_query]
+      regexp << Pattern::QUERY.to_s
+    end
+    
+    # optional fragment
+    if configuration[:allow_fragment]
+      regexp << "(" << Pattern::FRAGMENT.to_s << ")?"
+    end
+    
+    configuration[:with] = Regexp.new("^" << regexp << "$","i")
+    
     attr_names.each do |attr_name|
-      message = attr_name.to_s.match(/(_|\b)URL(_|\b)/i) ? DEFAULT_MESSAGE_URL : DEFAULT_MESSAGE
-      validates_format_of(attr_name, { :message => message }.merge(options))
+      validates_format_of(attr_name, configuration)
     end
   end
   
--- test/validates_url_format_of_test.rb
+++ test/validates_url_format_of_test.rb
@@ -28,11 +28,32 @@ class Model
     
   extend ValidatesUrlFormatOf
 
-  attr_accessor :homepage
-  validates_url_format_of :homepage
+  attr_accessor :url_complete
+  validates_url_format_of :url_complete
   
-  attr_accessor :my_UrL_hooray
-  validates_url_format_of :my_UrL_hooray
+  attr_accessor :url_simple
+  validates_url_format_of :url_simple,
+    :allow_ip       => false,
+    :allow_auth     => false,
+    :allow_domain   => true,
+    :allow_port     => false,
+    :allow_query    => false,
+    :allow_fragment => false
+  
+  attr_accessor :url_without_domain
+  validates_url_format_of :url_without_domain, :allow_domain => false
+  
+  attr_accessor :url_without_ip
+  validates_url_format_of :url_without_ip, :allow_ip => false
+  
+  attr_accessor :url_without_port
+  validates_url_format_of :url_without_port, :allow_port => false
+  
+  attr_accessor :url_without_query
+  validates_url_format_of :url_without_query, :allow_query => false
+  
+  attr_accessor :url_without_fragment
+  validates_url_format_of :url_without_fragment, :allow_fragment => false
   
   attr_accessor :custom_url
   validates_url_format_of :custom_url, :message => 'custom message'
@@ -40,76 +61,147 @@ end
 
 class ValidatesUrlFormatOfTest < Test::Unit::TestCase
   
-  def setup
-    @model = Model.new
+  def test_simple
+    assert_urls_valid :url_simple,
+      "http://example.com/"
+      "http://www.example.com/a/"
+      "https://www.example.com/a/b/a-index.html"
+    assert_urls_invalid :url_simple,
+      'http://192.168.2.1/',
+      'http://example.com/?abc',
+      'http://www.example.com/#foo',
+      'http://user:pass@example.com/'
+  end
+  
+  def test_without_ip
+    assert_urls_valid :url_without_ip, 
+      'http://example.com/',
+      'http://example.com:5321/',
+      'http://example.com/index',
+      'http://example.com/index?foo=bar&x=y',
+      'http://example.com/index#anchor',
+      'http://example.com/index?foo=bar&x=y#anchor'
+      
+    assert_urls_invalid :url_without_ip,
+      'http://1.1.1/'
+      'http://192.168.2.1/'
+  end
+  
+  def test_without_port
+    assert_urls_valid :url_without_port, 
+      'http://example.com/',
+      'http://1.1.1.1/index',
+      'http://123.123.123.123/index?foo=bar&x=y',
+      'http://example.com/index#anchor',
+      'http://example.com/index?foo=bar&x=y#anchor'
+      
+    assert_urls_invalid :url_without_port,
+      'http://example.com:/',
+      'http://1.1.1.1:1234/index',
+      'http://123.123.123.123:80/index?foo=bar&x=y',
+      'http://example.com:70000/index#anchor',
+      'http://example.com:999/index?foo=bar&x=y#anchor'
+  end
+  
+  def test_without_query
+    assert_urls_valid :url_without_query, 
+      'http://example.com/',
+      'http://1.1.1.1/index',
+      'http://example.com:421/index#anchor'
+      
+    assert_urls_invalid :url_without_query,
+      'http://example.com?',
+      'http://example.com/?',
+      'http://1.1.1.1:1234/index?a=b'
+  end
+  
+  def test_without_anchor
+    assert_urls_valid :url_without_fragment,
+      'http://example.com/',
+      'http://1.1.1.1/index',
+      'http://top:secret@example.com:421/stuff/?foo=bar'
+      
+    assert_urls_invalid :url_without_fragment,
+      'http://example.com#',
+      'http://example.com/#',
+      'http://1.1.1.1/index#chapter-2'
   end
   
   def test_should_allow_valid_urls
-    [
-      'http://example.com',
+    assert_urls_valid :url_complete,
       'http://example.com/',
       'http://www.example.com/',
       'http://sub.domain.example.com/',
-      'http://bbc.co.uk',
-      'http://example.com?foo',
-      'http://example.com?url=http://example.com',
-      'http://example.com:8000',
+      'http://bbc.co.uk/',
+      'http://example.com/?foo',
+      'http://example.com:8000/',
       'http://www.sub.example.com/page.html?foo=bar&baz=%23#anchor',
-      'http://user:pass@example.com',
-      'http://user:@example.com',
+      'http://user:pass@example.com/',
+      'http://user:@example.com/',
       'http://example.com/~user',
-      'http://example.xy',  # Not a real TLD, but we're fine with anything of 2-6 chars
-      'http://example.museum',
-      'http://1.0.255.249',
-      'http://1.2.3.4:80',
-      'HttP://example.com',
-      'https://example.com',
-      'http://räksmörgås.nu',  # IDN
-      'http://xn--rksmrgs-5wao1o.nu',  # Punycode
-      'http://example.com.',  # Explicit TLD root period
-      'http://example.com./foo'
-    ].each do |url|
-      @model.homepage = url
-      @model.valid?
-      assert @model.errors[:homepage].empty?, "#{url.inspect} should have been accepted"
-    end
+      'http://example.xy/',  # Not a real TLD, but we're fine with anything of 2-6 chars
+      'http://example.museum/',
+      'http://1.0.255.249/',
+      'http://1.2.3.4:80/',
+      'HttP://example.com/',
+      'https://example.com/',
+#      'http://räksmörgås.nu/',  # IDN
+      'http://xn--rksmrgs-5wao1o.nu/'  # Punycode
   end
   
   def test_should_reject_invalid_urls
-    [
+    assert_urls_invalid :url_complete,
       nil, 1, "", " ", "url",
+      'http://example.com',
       "www.example.com",
       "http://ex ample.com",
       "http://example.com/foo bar",
+      'http://example.com?foo',
+      'http://example.com:8000',
+      'http://user:pass@example.com',
+      'http://user:@example.com',
       'http://256.0.0.1',
       'http://u:u:u@example.com',
       'http://r?ksmorgas.com',
+      'http://example.com/?url=http://example.com',
+      
+      # Explicit TLD root period
+      'http://example.com.',
+      'http://example.com./foo',
       
       # These can all be valid local URLs, but should not be considered valid
       # for public consumption.
       "http://example",
       "http://example.c",
       'http://example.toolongtld'
-    ].each do |url|
-      @model.homepage = url
-      @model.valid?
-      assert !@model.errors[:homepage].empty?, "#{url.inspect} should have been rejected"
-    end
-  end
-  
-  def test_different_defaults_based_on_attribute_name
-    @model.homepage = 'x'
-    @model.my_UrL_hooray = 'x'
-    @model.valid?
-    assert_not_equal ValidatesUrlFormatOf::DEFAULT_MESSAGE, ValidatesUrlFormatOf::DEFAULT_MESSAGE_URL
-    assert_equal [ValidatesUrlFormatOf::DEFAULT_MESSAGE], @model.errors[:homepage]
-    assert_equal [ValidatesUrlFormatOf::DEFAULT_MESSAGE_URL], @model.errors[:my_UrL_hooray]
   end
   
   def test_can_override_defaults
-    @model.custom_url = 'x'
-    @model.valid?
-    assert_equal ['custom message'], @model.errors[:custom_url]
+    object = build_object(:custom_url, "x")
+    assert_equal ['custom message'], object.errors[:custom_url]
+  end
+  
+  private
+  
+  def build_object(attribute,value)
+    object = Model.new
+    object.send "#{attribute}=", value
+    object.valid?
+    object
+  end
+  
+  def assert_urls_valid(attribute, *urls)
+    for url in urls
+      object = build_object(attribute, url)
+      assert object.errors[attribute].empty?, "#{url.inspect} should have been accepted"
+    end
+  end
+  
+  def assert_urls_invalid(attribute, *urls)
+    for url in urls
+      object = build_object(attribute, url)
+      assert !object.errors[attribute].empty?, "#{url.inspect} should have been rejected"
+    end
   end
   
 end
